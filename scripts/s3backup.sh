ab5aedf85aeebbf7f8ca68692a0e207469fb5405
tmtabor <tabor@broadinstitute.org>
2017-03-27 13:30:33 -0700

    Update to s3 backup script

--- scripts/s3backup.sh
+++ scripts/s3backup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Set variables
-today=`date '+%Y_%m_%d'`
-backup="backup_$today"
+export today=`date '+%Y_%m_%d'`
+export backup="backup_$today"
 
 # Make the backups
 
 # Copy user notebooks
-aws s3 sync ~/users s3://gpnotebook-backup/$backup/users
+/usr/local/bin/aws s3 sync /home/thorin/users s3://gpnotebook-backup/$backup/users
 
 # Copy shared public notebooks
-aws s3 sync ~/repository/notebooks s3://gpnotebook-backup/$backup/repository/notebooks
+/usr/local/bin/aws s3 sync /home/thorin/repository/notebooks s3://gpnotebook-backup/$backup/repository/notebooks
 
 # Copy shared notebook database
-aws s3 cp ~/repository/webservice/db.sqlite3 s3://gpnotebook-backup/$backup/repository/webservice/db.sqlite3
+/usr/local/bin/aws s3 cp /home/thorin/repository/webservice/db.sqlite3 s3://gpnotebook-backup/$backup/repository/webservice/db.sqlite3

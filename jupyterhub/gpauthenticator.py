46d2fc1f1e3a51a6d9e474e42c0ede71f00623b7
tmtabor <tabor@broadinstitute.org>
2018-06-12 16:11:02 -0700

    Adding GenePatternGoogleAuthenticator

--- jupyterhub/gpauthenticator.py
+++ jupyterhub/gpauthenticator.py
@@ -12,24 +12,104 @@ from tornado import gen
 from tornado.httputil import url_concat
 from tornado.httpclient import HTTPRequest, AsyncHTTPClient, HTTPError
 from jupyterhub.auth import Authenticator, LocalAuthenticator
+from oauthenticator.google import GoogleOAuthenticator
+
+# URL of the GenePattern server you are authenticating with
+GENEPATTERN_URL = "https://genepattern.broadinstitute.org/gp"
+
+# Path to write authentication files to, for use with repo service authentication
+# Set to None to turn off writing authentication files
+REPO_AUTH_PATH = "/path/to/auth"
+
+# Path to the directory containing user notebook files
+# Set to None to turn off lazily creating user directories on authentication
+USERS_DIR_PATH = "/path/to/users"
+
+# Path to the directory containing the default notebooks to give new users
+# Set to None to skip copying any example notebooks
+DEFAULT_NB_DIR = "/path/to/defaults"
+
+
+class GenePatternGoogleAuthenticator(GoogleOAuthenticator):
+    @gen.coroutine
+    def authenticate(self, handler, data=None):
+        code = handler.get_argument("code")
+        handler.settings['google_oauth'] = {
+            'key': self.client_id,
+            'secret': self.client_secret,
+            'scope': self.scope,
+        }
+        user = yield handler.get_authenticated_user(
+            redirect_uri=self.get_callback_url(handler),
+            code=code)
+        access_token = str(user['access_token'])
+
+        http_client = handler.get_auth_http_client()
+
+        response = yield http_client.fetch(
+            self._OAUTH_USERINFO_URL + '?access_token=' + access_token
+        )
+
+        if not response:
+            handler.clear_all_cookies()
+            raise HTTPError(500, 'Google authentication failed')
+
+        bodyjs = json.loads(response.body.decode())
+
+        username = bodyjs['email']
+
+        if self.hosted_domain:
+            if not username.endswith('@' + self.hosted_domain) or \
+                    bodyjs['hd'] != self.hosted_domain:
+                raise HTTPError(403,
+                                "You are not signed in to your {} account.".format(
+                                    self.hosted_domain)
+                                )
+            else:
+                username = username.split('@')[0]
+
+        # If USERS_DIR_PATH is set, lazily create user directory
+        _create_user_directory(username)
+
+        # Attempt to call the scale up script
+        _autoscale()
+
+        return {
+            'name': username,
+            'auth_state': {
+                'access_token': access_token,
+                'google_user': bodyjs,
+            }
+        }
+
+
+def _autoscale():
+    # Attempt to call the scale up script
+    try:
+        subprocess.call(['/path/to/updateAutoscaleAMICount.py'])
+    except:
+        print('Could not call autoscale script.')
+
+
+def _create_user_directory(username):
+    if USERS_DIR_PATH is not None:
+        specific_user = os.path.join(USERS_DIR_PATH, username.lower())
+        if not os.path.exists(specific_user):
+            os.makedirs(specific_user)
+            os.chmod(specific_user, 0o777)
+
+            # Copy over example notebooks if USERS_DIR_PATH is set
+            if DEFAULT_NB_DIR is not None:
+                all_files = os.listdir(DEFAULT_NB_DIR)
+                for f in all_files:
+                    file_path = os.path.join(DEFAULT_NB_DIR, f)
+                    if os.path.isdir(file_path):
+                        shutil.copytree(file_path, os.path.join(specific_user, f))
+                    elif os.path.isfile(file_path):
+                        shutil.copy(file_path, specific_user)
 
 
 class GenePatternAuthenticator(Authenticator):
-    # URL of the GenePattern server you are authenticating with
-    GENEPATTERN_URL = "https://genepattern.broadinstitute.org/gp"
-
-    # Path to write authentication files to, for use with repo service authentication
-    # Set to None to turn off writing authentication files
-    REPO_AUTH_PATH = "/path/to/auth"
-
-    # Path to the directory containing user notebook files
-    # Set to None to turn off lazily creating user directories on authentication
-    USERS_DIR_PATH = "/path/to/users"
-
-    # Path to the directory containing the default notebooks to give new users
-    # Set to None to skip copying any example notebooks
-    DEFAULT_NB_DIR = "/path/to/defaults"
-
     @gen.coroutine
     def authenticate(self, handler, data):
         """Authenticate with GenePattern, and return the username if login is successful.
@@ -51,7 +131,7 @@ class GenePatternAuthenticator(Authenticator):
             client_id="GenePatternNotebook"
         )
 
-        url = url_concat(self.GENEPATTERN_URL + "/rest/v1/oauth2/token", params)
+        url = url_concat(GENEPATTERN_URL + "/rest/v1/oauth2/token", params)
 
         req = HTTPRequest(url,
                           method="POST",
@@ -69,39 +149,22 @@ class GenePatternAuthenticator(Authenticator):
             response_payload = json.loads(resp.body.decode("utf-8"))
 
             # If REPO_AUTH_PATH is set, write the authentication file
-            if self.REPO_AUTH_PATH is not None:
+            if REPO_AUTH_PATH is not None:
                 auth_dict = {
                     "username": username,
                     "token": response_payload['access_token'],
                     "timestamp": datetime.datetime.now().timestamp(),
                 }
-                auth_file = os.path.join(self.REPO_AUTH_PATH, username.lower() + '.json')
+                auth_file = os.path.join(REPO_AUTH_PATH, username.lower() + '.json')
                 f = open(auth_file, 'w')
                 f.write(json.dumps(auth_dict))
                 f.close()
 
             # If USERS_DIR_PATH is set, lazily create user directory
-            if self.USERS_DIR_PATH is not None:
-                specific_user = os.path.join(self.USERS_DIR_PATH, username.lower())
-                if not os.path.exists(specific_user):
-                    os.makedirs(specific_user)
-                    os.chmod(specific_user, 0o777)
-
-                    # Copy over example notebooks if USERS_DIR_PATH is set
-                    if self.DEFAULT_NB_DIR is not None:
-                        all_files = os.listdir(self.DEFAULT_NB_DIR)
-                        for f in all_files:
-                            file_path = os.path.join(self.DEFAULT_NB_DIR, f)
-                            if os.path.isdir(file_path):
-                                shutil.copytree(file_path, os.path.join(specific_user, f))
-                            elif os.path.isfile(file_path):
-                                shutil.copy(file_path, specific_user)
+            _create_user_directory(username)
 
             # Attempt to call the scale up script
-            try:
-                subprocess.call(['/path/to/updateAutoscaleAMICount.py'])
-            except:
-                print('Could not call autoscale script.')
+            _autoscale()
 
             # Return the username
             return {"name": username, "auth_state": {"access_token": response_payload['access_token']}}

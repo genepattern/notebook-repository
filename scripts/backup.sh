bfe964ca67d987e110fae75fd3649043ef8bbd4f
tmtabor <tabor@broadinstitute.org>
2019-07-29 11:34:06 -0400

    Update to backup cron

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -6,12 +6,5 @@ export backup="backup_$today"
 
 # Make the backups
 
-# Copy user notebooks
-aws s3 sync /data/users s3://gpnotebook-backup/$backup/users
-
-# Copy shared public notebooks
-aws s3 sync /data/repository s3://gpnotebook-backup/$backup/repository
-aws s3 sync /data/shared s3://gpnotebook-backup/$backup/shared
-
-# Copy shared notebook database
-aws s3 cp /data/db.sqlite3 s3://gpnotebook-backup/$backup/db.sqlite3
+# Copy the data directory
+/home/ubuntu/.local/bin/aws s3 sync /data s3://gpnotebook-backup/$backup

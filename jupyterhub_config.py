799b1e09e37060248728067e594ea3889197a498
tmtabor <tmtabor@cloud.ucsd.edu>
2020-11-16 10:04:47 -0800

    Moved projects to JupyterHub home page

--- jupyterhub_config.py
+++ jupyterhub_config.py
@@ -6,12 +6,12 @@ from jupyterhub.handlers import BaseHandler
 from tornado.web import authenticated
 
 
-class ProjectsHandler(BaseHandler):
-    """Serve the projects page from its template: theme/templates/projects.html"""
-
-    @authenticated
-    def get(self):
-        self.write(self.render_template('projects.html'))
+# class ProjectsHandler(BaseHandler):
+#     """Serve the projects page from its template: theme/templates/projects.html"""
+#
+#     @authenticated
+#     def get(self):
+#         self.write(self.render_template('projects.html'))
 
 
 c = get_config()
@@ -44,8 +44,8 @@ copy_tree('./theme/images/', os.path.join(static_path, 'images'), update=1)
 
 # Named server config
 c.JupyterHub.allow_named_servers = True
-c.JupyterHub.default_url = '/projects'
-c.JupyterHub.extra_handlers = [('projects', ProjectsHandler)]
+c.JupyterHub.default_url = '/home'
+#c.JupyterHub.extra_handlers = [('projects', ProjectsHandler)]
 c.DockerSpawner.name_template = "{prefix}-{username}-{servername}"
 
 # Ensure that JupyterHub can connect to the singleuser images

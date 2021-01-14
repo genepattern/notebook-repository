7a8fceed3d73d2388b2e10bfd17cc13e8a5b891a
tmtabor <tmtabor@cloud.ucsd.edu>
2021-01-14 10:51:16 -0800

    Update to backup script

--- scripts/backup.sh
+++ scripts/backup.sh
@@ -2,9 +2,10 @@
 
 # Set variables
 export today=`date '+%Y_%m_%d'`
-export backup="backup_$today"
+export backup="workspace_backup_$today"
 
-# Make the backups
-
-# Copy the data directory
+# Make the backup, copy the data directory
 /home/ubuntu/.local/bin/aws s3 sync /data s3://gpnotebook-backup/$backup
+
+# Remove the old backups
+# /home/ubuntu/.local/bin/aws s3 ls s3://gpnotebook-backup | awk '{print $2}' | grep -v $backup | grep -E -i 'workspace_backup_' | xargs -I% bash -c 'aws s3 rm --recursive s3://gpnotebook-backup/%'

97e985510fc4e1ed6c4f40bf8790acd3a6f71ed9
tmtabor <tmtabor@cloud.ucsd.edu>
2021-01-15 13:05:08 -0800

    Another authenticator fix for different versions of tornado

--- gpauthenticator/handlers.py
+++ gpauthenticator/handlers.py
@@ -1,6 +1,7 @@
 import os
-from jupyterhub.handlers import  LoginHandler, LogoutHandler
+from jupyterhub.handlers import LoginHandler, LogoutHandler, url_escape
 from tornado.httpclient import AsyncHTTPClient, HTTPRequest, HTTPError
+from tornado.httputil import url_concat
 
 TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates')
 
@@ -8,7 +9,7 @@ TEMPLATE_DIR = os.path.join(os.path.dirname(__file__), 'templates')
 class LoginHandler(LoginHandler):
     """Handle the login form and render the login page."""
 
-    async def get(self):
+    async def get(self, login_error=None):
         self.statsd.incr('login.request')
         user = self.current_user
         if user:
@@ -17,11 +18,22 @@ class LoginHandler(LoginHandler):
             self.set_login_cookie(user)
             self.redirect(self.get_next_url(user), permanent=False)
         else:
-            template = self.render_template('login.html')
+            username = self.get_argument('username', default='')
+            template = self.render_template(
+                'login.html',
+                next=url_escape(self.get_argument('next', default='')),
+                username=username,
+                login_error=login_error,
+                custom_html=self.authenticator.custom_html,
+                login_url=self.settings['login_url'],
+                authenticator_login_url=url_concat(
+                    self.authenticator.login_url(self.hub.base_url),
+                    {'next': self.get_argument('next', '')},
+                ),
+            )
             if not isinstance(template, str):
                 template = await template
-            username = self.get_argument('username', default='')
-            self.write(self._render(username=username))
+            self.write(template)
 
 
 class LogoutHandler(LogoutHandler):
